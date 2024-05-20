c7a2d28b4206fb338a12eab6e9fa7017d5c4acba
Akbarshax <akbarshax.lp@gmail.com>
2024-05-20 10:53:13 +0500

    rm unnecessary blocks, and conflicts

--- es6.js
+++ es6.js
@@ -30,8 +30,8 @@ console.log(`Hello, ${name}!`);
 // 4. Расширение объектных литералов: Возможность использовать
 // вычисляемые имена свойств, укороченный синтаксис методов
 // и другие улучшения.
-let a = 5, b = 10;
-let obj = { a, b };
+let a1 = 5, b1 = 10;
+let obj1 = { a1, b1 };
 
 let propName = 'age';
 let person = {
@@ -77,10 +77,8 @@ dog.speak(); // "Rex barks."
 export function sum(a, b) {
   return a + b;
 }
-
 // Импорт модуля
 import { sum } from './math.js';
-
 
 // 8. Promise: Стандартный способ работы с асинхронным кодом,
 // представляющий результат выполнения операции, которая
@@ -97,31 +95,7 @@ asyncFunction().then((result) => {
   console.log(result); // "Async complete"
 });
 
-
-// 9. Итераторы и генераторы: Итераторы обеспечивают стандартный
-// способ перебора элементов, а генераторы позволяют создавать
-// итераторы с помощью нового ключевого слова function*.
-let iterable = {
-  [Symbol.iterator]() {
-    let i = 0;
-    return {
-      next() {
-        if (i < 3) {
-          return { value: i++, done: false };
-        } else {
-          return { done: true };
-        }
-      }
-    };
-  }
-};
-
-for (let value of iterable) {
-  console.log(value); // 0, 1, 2
-}
-
-
-// 10. Map и Set: Новые коллекции для хранения уникальных значений.
+// 9. Map и Set: Новые коллекции для хранения уникальных значений.
 let map = new Map();
 map.set('key', 'value');
 console.log(map.get('key')); // "value"
@@ -129,26 +103,4 @@ console.log(map.get('key')); // "value"
 let set = new Set([1, 2, 3, 4, 4, 5]);
 console.log(set.size); // 5
 
-
-// 11. Proxy и Reflect: Proxy позволяет создавать объекты-заместители для
-// других объектов, позволяя перехватывать и изменять их поведение. Reflect
-// предоставляет методы для выполнения стандартных операций над объектами,
-// таких как чтение, запись и удаление свойств.
-let target = {};
-let handler = {
-  get(target, prop, receiver) {
-    console.log(`Getting ${prop}`);
-    return Reflect.get(target, prop, receiver);
-  },
-  set(target, prop, value, receiver) {
-    console.log(`Setting ${prop} to ${value}`);
-    return Reflect.set(target, prop, value, receiver);
-  }
-};
-let proxy = new Proxy(target, handler);
-
-proxy.foo = 'bar'; // Setting foo to bar
-console.log(proxy.foo); // Getting foo, "bar"
-
-
-// 12. И многое другое, такие как: (find, includes, forEach)
+// 10. И многое другое, такие как: (find, includes, forEach)
